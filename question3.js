1e8fe568817e375a1d94af697af52c03bd20217e
Valerie Pilossof <35698300+valpil@users.noreply.github.com>
2018-06-12 20:55:20 -0400

    Update question3.js

--- question3.js
+++ question3.js
@@ -1,8 +1,8 @@
 function changePossibilities(amount, denominations) {
   let counter = 0;
   for (let i = 0; i < denominations.length; i++) {
-    let currentElem = denominations[i];
-    let target = amount - currentElem;
+    const currentElem = denominations[i];
+    const target = amount - currentElem;
     if (amount === currentElem) {
       counter++;
       break;
